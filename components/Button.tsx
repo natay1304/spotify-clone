f640407663ed19737cd590c7df38719e85780aec
Anastasiya <amalyutina@melashore.games>
2025-05-22 15:09:29 +0400

    supabse added

---
+++ components/Button.tsx
@@ -0,0 +1,49 @@
+import { forwardRef } from "react"
+import { twMerge } from "tailwind-merge";
+
+// eslint-disable-next-line @typescript-eslint/no-empty-object-type
+interface ButtonProps 
+    extends React.ButtonHTMLAttributes<HTMLButtonElement> {}
+
+
+const Button = forwardRef<HTMLButtonElement, ButtonProps>((
+    {
+      className,
+      children,
+      disable,
+      type ="button",
+      ...props
+    }, ref) => 
+      {
+        return(
+            <button
+              type={type}
+              className={twMerge(`
+                w-full
+                rounded-full
+                bg-green-500
+                border
+                border-transparent
+                px-3
+                py-3
+                disabled:cursor-not-allowed
+                disabled:opacity-50
+                text-black
+                font-bold
+                hover:opacity-75
+                transition
+                `,
+              className
+            )}
+            disabled={disable}
+            ref={ref}
+            {...props}
+            >
+              {children}
+            </button>
+          )
+  })
+
+Button.displayName = "Button";
+
+export default Button;
